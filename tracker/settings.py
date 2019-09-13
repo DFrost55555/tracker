6df3ba3f0d02fdce7374c41f61f3c29e365445ab
DFrost55555 <darren.frost55555@outlook.com>
2019-09-13 15:18:24 +0100

    Update settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'HOST': 'gd-adm-mssql01.database.windows.net',
         'PORT':'1433',
         'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',
+            'driver': 'SQL Server Native Client 11.0',
                 }
     }
 }
