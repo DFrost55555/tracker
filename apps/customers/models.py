5c1fec49bce2d5a872da17362be95c6ef0ea0eb3
DFrost55555 <darren.frost55555@outlook.com>
2019-09-23 16:04:15 +0100

    Update models.py

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 class Customer(models.Model):
-    cust_name = models.CharField(max_length=150, verbose_name = "Customer Name")
+    cust_name = models.CharField(max_length=150)
     cust_createdby = models.ForeignKey(User, on_delete=models.CASCADE)
     cust_createddate = models.DateTimeField(default=timezone.now)
     cust_modifiedby = models.ForeignKey(User, related_name='editor',on_delete=models.CASCADE)
