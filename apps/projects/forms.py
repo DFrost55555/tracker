d46a0beead8b4bd73fabf2847fb99985cea4a664
DFrost55555 <darren.frost55555@outlook.com>
2019-09-27 12:16:53 +0100

    Create forms.py

---
+++ apps/projects/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Project
+from apps.customers.models import Customer
+from django.forms import ModelChoiceField
+
+
+class ProjectModelForm(forms.ModelForm):
+    project_name = forms.CharField(widget=forms.TextInput(), required=True)
+    project_customer_fk = ModelChoiceField(queryset=Customer.objects.all(), initial=0, required=True)
+    
+    class Meta:
+        model = Project
+        fields = [
+            "project_name",
+            "project_customer_fk",
+        ]
+        
