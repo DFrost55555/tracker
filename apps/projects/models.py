ab9a52f91fe87ab09b4432ebc28085e427fbb1b4
DFrost55555 <darren.frost55555@outlook.com>
2019-09-27 12:44:00 +0100

    Update models.py

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class Project(models.Model):
     project_name = models.CharField('Project Name', max_length=150)
-    project_customer_fk = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    project_customer_fk = models.ForeignKey('Customer',Customer, on_delete=models.SET_NULL, null=True)
     project_createdby = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     project_createddate = models.DateTimeField(default=timezone.now)
     project_modifiedby = models.ForeignKey(User, related_name='prj_editor',on_delete=models.SET_NULL, null=True)
