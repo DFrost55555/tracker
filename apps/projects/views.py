7a5eb94d398879473920ca982d311961c8ec8ddc
DFrost55555 <darren.frost55555@outlook.com>
2019-09-27 12:17:06 +0100

    Update views.py

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User, Group
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .forms import ProjectModelForm
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+from apps.customers.models import Customer
 
 def prj_home(request):
     return render(request, 'projects/prj_home.html', {'title': 'Projects Home'})
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    fields = ['project_name', 'project_customer_fk']
+    
+    def form_valid(self, form):
+        form.instance.project_createdby = self.request.user
+        form.instance.project_modifiedby = self.request.user
+        return super().form_valid(form)
