5b34a2f95f738c4a501260a3ef63154e861d473c
DFrost55555 <darren.frost55555@outlook.com>
2019-09-27 14:23:42 +0100

    Update urls.py

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -25,4 +25,8 @@ from .views import (
 )
 urlpatterns = [
     path('projects/', ProjectFilterView, name='project-home'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('project/new/', ProjectCreateView.as_view(), name='project-create'),
+    #path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project-update'),
+    #path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project-delete'),
 ]
