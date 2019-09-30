fa26ef19563d18cc893620b5d95320cc359f5e28
DFrost55555 <darren.frost55555@outlook.com>
2019-09-30 16:40:24 +0100

    Multiple file updates 16:40

---
+++ apps/statustype/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class StatusType(models.Model):
+    statustype_name = models.CharField('Service Area Name', max_length=150)
+    statustype_description = models.CharField('Description', max_length=2000)
+    statustype_createdby = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    statustype_createddate = models.DateTimeField(default=timezone.now)
+    statustype_modifiedby = models.ForeignKey(User, related_name='statustype_editor',on_delete=models.SET_NULL, null=True)
+    statustype_modifieddate = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.statustype_name
