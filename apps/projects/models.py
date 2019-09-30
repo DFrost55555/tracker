50d8b8dfb4a24afd516d355c034862fefb6fe902
DFrost55555 <darren.frost55555@outlook.com>
2019-09-30 16:57:13 +0100

    Multiple Files updates DF 16:57

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from apps.customers.models import Customer
-#from apps.statustype.models import StatusType
+from apps.statustype.models import StatusType
 from django.urls import reverse
 
 
@@ -12,14 +12,13 @@ CHARGE_CODE_CHOICES = (
     ('trs','MARS Request')
 )
 
-
 class Project(models.Model):
     project_name = models.CharField('Project Name', max_length=150)
     project_customer_fk = models.ForeignKey(Customer, verbose_name='Customer', on_delete=models.SET_NULL, null=True)
     project_reference = models.CharField('Project Reference', max_length=255)
     project_chargecode = models.CharField('Charge Code',max_length=255)
     project_chargecodetype = models.CharField('Charge Code Type', max_length=25, choices=CHARGE_CODE_CHOICES, default='wbs')
-    #project_statustype_fk = models.ForeignKey(StatusType, verbose_name='Status', on_delete=models.SET_NULL, null=True)
+    project_statustype_fk = models.ForeignKey(StatusType, verbose_name='Status', on_delete=models.SET_NULL, null=True)
     project_createdby = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     project_createddate = models.DateTimeField(default=timezone.now)
     project_modifiedby = models.ForeignKey(User, related_name='prj_editor',on_delete=models.SET_NULL, null=True)
