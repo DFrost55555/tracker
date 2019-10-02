823e30155012c55d15eb0a1eab2a415236e1caeb
DFrost55555 <darren.frost55555@outlook.com>
2019-10-02 12:11:21 +0100

    Update admin.py

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -4,3 +4,7 @@ from .models import Customer, CustomerContact, CustomerNote
 admin.site.register(Customer)
 admin.site.register(CustomerContact)
 admin.site.register(CustomerNote)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('custcontact_firstname', 'custcontact_lastname','custcontact_role','custcontact_mobnumber','custcontact_email')
+    
