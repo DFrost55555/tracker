25c3297da13c1a79c7be1e7ca8bc9af5383bc755
DFrost55555 <darren.frost55555@outlook.com>
2019-10-02 12:15:33 +0100

    Update admin.py

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Customer, CustomerContact, CustomerNote
 
-admin.site.register(Customer)
-admin.site.register(CustomerContact)
-admin.site.register(CustomerNote)
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('custcontact_firstname', 'custcontact_lastname','custcontact_role','custcontact_mobnumber','custcontact_email')
-    
+
+
+admin.site.register(Customer, CustomerContact, CustomerNote, CustomerAdmin)    
