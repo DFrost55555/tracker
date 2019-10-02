1c86bfe113f7d4c1cbb22432bb983c35fc985085
DFrost55555 <darren.frost55555@outlook.com>
2019-10-02 12:37:43 +0100

    Update admin.py

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -4,7 +4,8 @@ from .models import Customer, CustomerContact, CustomerNote
 
 class CustomerContactAdmin(admin.ModelAdmin):
     list_display = ('custcontact_firstname', 'custcontact_lastname','custcontact_role','custcontact_mobnumber','custcontact_email')
-
+    list_display_links = ('custcontact_firstname', 'custcontact_lastname')
+    list_per_page = 15
 
 admin.site.register(Customer)
 admin.site.register(CustomerContact, CustomerContactAdmin)
