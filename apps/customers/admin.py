db6afddd4ad5dc7c688ae36d4ccb1d710f49c41c
DFrost55555 <darren.frost55555@outlook.com>
2019-10-02 11:32:04 +0100

    DF 11:31

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, CustomerContact, CustomerNote
 
 admin.site.register(Customer)
+admin.site.register(CustomerContact)
+admin.site.register(CustomerNote)
--- apps/customers/models.py
+++ apps/customers/models.py
@@ -16,3 +16,35 @@ class Customer(models.Model):
     def get_absolute_url(self):
         return reverse ('customer-detail', kwargs={"pk": self.pk})
     
+    
+class CustomerContact(models.Model):
+    custcontact_customer_fk = models.ForeignKey(Customer, verbose_name='Customer', on_delete=models.SET_NULL, null=True)
+    custcontact_firstname = models.CharField('First Name', max_length=150)
+    custcontact_lastname = models.CharField('Last Name', max_length=150)
+    custcontact_email = models.EmailField('Email Address', max_length=250)
+    custcontact_telnum = models.CharField('Telephone Number', max_length=150)
+    custcontact_mobnumber = models.CharField('Mobile Number', max_length=150)
+    custcontact_createdby = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    custcontact_createddate = models.DateTimeField(default=timezone.now)
+    custcontact_modifiedby = models.ForeignKey(User, related_name='custcontact_editor', on_delete=models.SET_NULL, null=True)
+    custcontact_modifieddate = models.DateTimeField(auto_now=True, null=True)
+    
+    def __str__(self):
+            return self.custcontact_email
+
+    def get_absolute_url(self):
+        return reverse ('custcontact-detail', kwargs={"pk": self.pk})
+    
+class CustomerNote(models.Model):
+    custnote_customer_fk = models.ForeignKey(Customer, verbose_name='Customer', on_delete=models.SET_NULL, null=True)
+    custnote_note = models.CharField('Note', max_length=2500)
+    custnote_createdby = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    custnote_createddate = models.DateTimeField(default=timezone.now)
+    custnote_modifiedby = models.ForeignKey(User, related_name='custnote_editor', on_delete=models.SET_NULL, null=True)
+    custnote_modifieddate = models.DateTimeField(auto_now=True, null=True)
+    
+    def __str__(self):
+            return self.custnote_note
+
+    def get_absolute_url(self):
+        return reverse ('custnote-detail', kwargs={"pk": self.pk})
