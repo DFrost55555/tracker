190da67707e24074d5bbba1b049284a66f10c3ce
DFrost55555 <darren.frost55555@outlook.com>
2019-10-02 17:18:19 +0100

    Update filters.py

--- apps/customers/filters.py
+++ apps/customers/filters.py
@@ -9,11 +9,13 @@ class CustomerFilter(filters.FilterSet):
         model = Customer
         fields = {
             'cust_name': ['icontains']
-                }  
+        }  
 
 
 class CustomerProjectFilter(filters.FilterSet):
     
     class Meta:
         model = Project
-        fields = ('project_customer_fk')
+        fields = {
+            'project_customer_fk': self.object.pk
+        }
