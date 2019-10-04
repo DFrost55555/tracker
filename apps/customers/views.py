ad03c0bd9891bc3b9012f9918d703bc73b47a5ad
DFrost55555 <darren.frost55555@outlook.com>
2019-10-04 10:20:43 +0100

    Update views.py

--- apps/customers/views.py
+++ apps/customers/views.py
@@ -37,7 +37,7 @@ class CustomerDetailView(LoginRequiredMixin, DetailView):
     model = Customer
     
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(self, **kwargs)
+        context = super().get_context_data(**kwargs)
         context['project_list'] = Project.objects.all()
         return context
         
