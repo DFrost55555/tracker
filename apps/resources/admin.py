aa5272a1fdfadd8f8c8b8ee4efe731819ae95f0e
DFrost55555 <darren.frost55555@outlook.com>
2019-10-08 16:01:39 +0100

    Update admin.py

--- apps/resources/admin.py
+++ apps/resources/admin.py
@@ -5,7 +5,7 @@ from .models import Resource, ResourceType
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ('res_firstname', 'res_lastname', 'res_resource_type')
     list_per_page = 20
-    ordering = ('res_lastname')
+    ordering = ('res_lastname',)
 
 admin.site.register(Resource, ResourceAdmin)
 admin.site.register(ResourceType)
