7eb50ad21b2c4e971ee5065b89bca1b5e0e340c8
DFrost55555 <darren.frost55555@outlook.com>
2019-10-08 13:56:59 +0100

    DF 13:56

--- apps/roles/models.py
+++ apps/roles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Role(models.Model):
+    role_id = models.AutoField(primary_key=True)
+    role_name = models.CharField('role name', max_length=100)
+    role_desc = models.CharField('role description', max_length=2000)
+    role_createdby = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    role_createddate = models.DateTimeField(default=timezone.now)
+    role_modifiedby = models.ForeignKey(User, related_name='role_editor',on_delete=models.SET_NULL, null=True)
+    role_modifieddate = models.DateTimeField(auto_now=True, null=True)
+    
+    def __str__(self):
+            return self.role_name
