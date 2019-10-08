b0ea171554919d48f4bc0821f62a92f6df449565
DFrost55555 <darren.frost55555@outlook.com>
2019-10-08 15:49:03 +0100

    Update admin.py

--- apps/resources/admin.py
+++ apps/resources/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import Resource, ResourceType
 
-
-class ResourceAdmin(admin.ModelAdmin):
-    list_display = ('res_lastname', 'res_firstname')
-    list_per_page = 20
-    ordering = ('res_lastname')
-
-admin.site.register(Resource, ResourceAdmin)
+admin.site.register(Resource)
 admin.site.register(ResourceType)
