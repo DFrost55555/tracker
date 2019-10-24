31f697b260024d9f5ed6e1a3b5c83c088837f22d
DFrost55555 <darren.frost55555@outlook.com>
2019-10-24 10:22:49 +0100

    Update views.py

--- apps/customers/views.py
+++ apps/customers/views.py
@@ -45,7 +45,7 @@ class CustomerDetailView(LoginRequiredMixin, DetailView):
         'cust_contacts' : CustomerContact.objects.filter(custcontact_customer_fk=self.kwargs['pk']),
         })
         return context
-    
+        self.request.session['cust_id'] = self.object.id
             
 class CustomerCreateView(LoginRequiredMixin, CreateView):
     model = Customer
@@ -64,24 +64,9 @@ class CustomerUpdateView(LoginRequiredMixin, UpdateView):
     fields = ['cust_name']
     
     def form_valid(self, form):
-        #form.instance.cust_createdby = self.request.user
         form.instance.cust_modifiedby = self.request.user
         return super().form_valid(form)
     
 class CustomerDeleteView(LoginRequiredMixin, DeleteView):
     model = Customer
     success_url = '/'
-
-
-
-"""     model = Customer
-    template_name = 'customers/cust_home.html'
-    context_object_name = 'customers'
-    ordering = ['cust_name']
-    paginate_by = 10
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['filter'] = CustomerFilter(self.request.GET,queryset=self.get_queryset())
-        return context """
-        
