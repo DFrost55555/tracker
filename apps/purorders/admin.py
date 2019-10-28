da8eaaf60c44033eace8565005112b4835c73aca
DFrost55555 <darren.frost55555@outlook.com>
2019-10-28 10:59:55 +0000

    Update admin.py

--- apps/purorders/admin.py
+++ apps/purorders/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import PurchaseOrder, POMatrix, PONote, POStatus, POType
+from .models import PurchaseOrder, POMatrix, PONote, POStatus, POType, POFiles
 
 admin.site.register(PurchaseOrder)
 admin.site.register(POMatrix)
 admin.site.register(PONote)
 admin.site.register(POStatus)
 admin.site.register(POType)
+admin.site.register(POFiles)
