31babd4cf4806a285f44563acf58e4bcb68d20d2
DFrost55555 <darren.frost55555@outlook.com>
2019-11-07 17:18:59 +0000

    Update admin.py

--- apps/vendors/admin.py
+++ apps/vendors/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Vendor,VendorContact,VendorNote,VendorType
 
 # Register your models here.
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('vend_name')
+    list_per_page = 20
+    ordering = ('vend_name',)
 
-admin.site.register(Vendor)
+
+admin.site.register(Vendor, VendorAdmin)
 admin.site.register(VendorContact)
 admin.site.register(VendorNote)
 admin.site.register(VendorType)
