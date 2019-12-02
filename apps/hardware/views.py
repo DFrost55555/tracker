996c61e8ae6dc9fcd568d844981930463b10434f
DFrost55555 <darren.frost55555@outlook.com>
2019-12-02 12:57:37 +0000

    Update views.py

--- apps/hardware/views.py
+++ apps/hardware/views.py
@@ -48,7 +48,7 @@ class HardwareDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(HardwareDetailView, self).get_context_data(**kwargs)
-        self.request.session['hardware_id'] = self.object.id
+        self.request.session['hardware_id'] = self.object.hw_id
         return context
     
 
