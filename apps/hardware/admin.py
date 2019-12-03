032a97ce387f20234938c4dc86b6442172924329
DFrost55555 <darren.frost55555@outlook.com>
2019-12-03 10:08:37 +0000

    df 10:07

--- apps/hardware/admin.py
+++ apps/hardware/admin.py
@@ -3,9 +3,15 @@ from .models import PortfolioStatus,Hardware,HardwareContact,HardwareNote, Hardw
 
 
 # Register your models here.
+
+class HardwareMatrixAdmin(admin.ModelAdmin):
+    list_display = ('hwmtx_id', 'hwmtx_hw_fk','hwmtx_cust_fk','hwmtx_portsts_fk','hwmtx_portcat_fk')
+    list_display_links = ('hwmtx_hw_fk')
+    list_per_page = 15
+
 admin.site.register(PortfolioStatus)
 admin.site.register(PortfolioCategory)
 admin.site.register(Hardware)
 admin.site.register(HardwareContact)
 admin.site.register(HardwareNote)
-admin.site.register(HardwareMatrix)
+admin.site.register(HardwareMatrix, HardwareMatrixAdmin)
