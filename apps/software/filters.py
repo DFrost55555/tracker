1ce40c21e4e31870a656517892016018e24e0c99
DFrost55555 <darren.frost55555@outlook.com>
2020-01-06 16:48:56 +0000

    df 16:48

--- apps/software/filters.py
+++ apps/software/filters.py
@@ -3,11 +3,10 @@ from django_filters import DateFilter, CharFilter
 from django.utils.translation import gettext_lazy as _
 from .models import Software,SoftwareVendor,SoftwareMatrix
 from apps.lists.models import ProductType,SoftwareCategory,SoftwareStatus
-from apps.vendors.models import Vendor
 
 class SoftwareFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name='sw_eol_date', lookup_expr='gte', label='EOS Greater Than')
-    end_date = DateFilter(field_name='sw_eol_date', lookup_expr='lte',  label='EOS Less Than')
+    start_date = DateFilter(field_name='sw_eol_date', lookup_expr='gte', label='EOS After')
+    end_date = DateFilter(field_name='sw_eol_date', lookup_expr='lte',  label='EOS Before')
     sw_description = CharFilter(field_name='sw_description', lookup_expr='icontains', label='Description')
     sw_version = CharFilter(field_name='sw_version', lookup_expr='icontains', label='Version')
     class Meta:
@@ -16,6 +15,6 @@ class SoftwareFilter(django_filters.FilterSet):
             'sw_vend_fk',
             'sw_description',
             'sw_version'
-                }  
+            }  
 
   
