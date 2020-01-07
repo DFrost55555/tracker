3f2b70c9286cf0ea3df54ff3b2afe8416a6fea40
DFrost55555 <darren.frost55555@outlook.com>
2020-01-07 10:38:32 +0000

    df 10:38

--- apps/software/filters.py
+++ apps/software/filters.py
@@ -1,14 +1,15 @@
 import django_filters
 from django_filters import DateFilter, CharFilter
 from django.utils.translation import gettext_lazy as _
+from django.forms.widgets import TextInput
 from .models import Software,SoftwareVendor,SoftwareMatrix
 from apps.lists.models import ProductType,SoftwareCategory,SoftwareStatus
 
 class SoftwareFilter(django_filters.FilterSet):
     start_date = DateFilter(field_name='sw_eol_date', lookup_expr='gte', label='EOS After')
     end_date = DateFilter(field_name='sw_eol_date', lookup_expr='lte',  label='EOS Before')
-    sw_description = CharFilter(field_name='sw_description', lookup_expr='icontains', label='Description')
-    sw_version = CharFilter(field_name='sw_version', lookup_expr='icontains', label='Version')
+    sw_description = CharFilter(field_name='sw_description', lookup_expr='icontains', label='Product', widget=TextInput(attrs={'placeholder': 'Product name contains...'}))
+    sw_version = CharFilter(field_name='sw_version', lookup_expr='icontains', label='Version', widget=TextInput(attrs={'placeholder': 'Version contains...'}))
     class Meta:
         model = Software
         fields = {
