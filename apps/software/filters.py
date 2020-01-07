37f8919f8c605d08115fb3874e47e40311839b67
DFrost55555 <darren.frost55555@outlook.com>
2020-01-07 14:24:15 +0000

    df 14:24

--- apps/software/filters.py
+++ apps/software/filters.py
@@ -1,13 +1,16 @@
 import django_filters
 from django_filters import DateFilter, CharFilter
 from django.utils.translation import gettext_lazy as _
-from django.forms.widgets import TextInput
+from django.forms.widgets import TextInput, DateInput, NumberInput
 from .models import Software,SoftwareVendor,SoftwareMatrix
 from apps.lists.models import ProductType,SoftwareCategory,SoftwareStatus
 
+class DatePicker(forms.DateInput):
+    input_type = 'date'
+
 class SoftwareFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name='sw_eol_date', lookup_expr='gte', label='EOS After')
-    end_date = DateFilter(field_name='sw_eol_date', lookup_expr='lte',  label='EOS Before')
+    start_date = DateFilter(field_name='sw_eol_date', lookup_expr='gte', label='EOS After', widget=DatePicker())
+    end_date = DateFilter(field_name='sw_eol_date', lookup_expr='lte',  label='EOS Before', widget=DatePicker())
     sw_description = CharFilter(field_name='sw_description', lookup_expr='icontains', label='Product', widget=TextInput(attrs={'placeholder': 'Product name contains...'}))
     sw_version = CharFilter(field_name='sw_version', lookup_expr='icontains', label='Version', widget=TextInput(attrs={'placeholder': 'Version contains...'}))
     class Meta:
@@ -15,7 +18,10 @@ class SoftwareFilter(django_filters.FilterSet):
         fields = {
             'sw_vend_fk',
             'sw_description',
-            'sw_version'
+            'sw_version',
+            'sw_eol_date',
+            'start_date',
+            'end_date'
             }  
 
   
