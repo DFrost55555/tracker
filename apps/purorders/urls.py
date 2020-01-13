cd2505019784a55a98f8845ae55f55c9f86a53a8
DFrost55555 <darren.frost55555@outlook.com>
2020-01-13 15:19:30 +0000

    df 15:13

---
+++ apps/purorders/urls.py
@@ -0,0 +1,34 @@
+"""django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from . import views
+from .views import (
+    PurOrdersFilterView,
+    PurOrdersDetailView,
+    PurOrdersCreateView,
+    PurOrdersUpdateView,
+    PurOrdersDeleteView,
+    #CustProjectCreateView,
+)
+urlpatterns = [
+    path('purorders/', PurOrdersFilterView, name='purorders-home'),
+    path('purorders/<int:pk>/', PurOrdersDetailView.as_view(), name='purorders-detail'),
+    path('purorders/new/', PurOrdersCreateView.as_view(), name='purorders-create'),
+    path('purorders/<int:pk>/update/', PurOrdersUpdateView.as_view(), name='purorders-update'),
+    path('purorders/<int:pk>/delete/', PurOrdersDeleteView.as_view(), name='purorders-delete'),
+    #path('project/new/customer/', CustProjectCreateView.as_view(), name='project-create-cust'),
+]
--- apps/purorders/views.py
+++ apps/purorders/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User, Group
+from django.contrib import messages
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .forms import ProjectModelForm, CustProjectModelForm
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Project
+from apps.customers.models import Customer
+from apps.statustype.models import StatusType
+from apps.chgcodetype.models import ChgCodeType
+from apps.lists.models import Location, ProjectStatus, ResourceStatus, ChargeCodeType, ChargeUnitType
+from .filters import ProjectFilter
 
-# Create your views here.
+def PurOrdersFilterView(request):
+    qs = PurchaseOrder.objects.all().order_by('po_reference')
+    poReference_query = request.GET.get('poReference')
+    
+    if poReference_query != '' and poReference_query is not None:
+        qs = qs.filter(po_reference__icontains=poReference_query).order_by('po_reference')
+            
+    paginator = Paginator(qs, 10)
+    
+    page = request.GET.get('page')
+    
+    qs = paginator.get_page(page)
+    
+    context = {
+        'queryset': qs
+    }
+    
+    return render(request,"purorders/purorders_home.html",context)
+
+
+class PurOrdersDetailView(LoginRequiredMixin, DetailView):
+    model = PurchaseOrder
+    
+
+class PurOrdersCreateView(LoginRequiredMixin, CreateView):
+    model = PurchaseOrder
+    fields = ['po_reference', 'po_quantity', 'po_quantity_type_fk', 'po_cost_value', 'po_unit_cost', 'po_charge_value', 'po_unit_charge', 'po_start_date', 'po_end_date', 'po_status_fk']
+    
+    def form_valid(self, form):
+        form.instance.po_createdby = self.request.user
+        form.instance.po_modifiedby = self.request.user
+        return super().form_valid(form)
+    
+        
+class PurOrdersUpdateView(LoginRequiredMixin, UpdateView):
+    model = PurchaseOrder
+    fields = ['po_reference', 'po_quantity', 'po_quantity_type_fk', 'po_cost_value', 'po_unit_cost', 'po_charge_value', 'po_unit_charge', 'po_start_date', 'po_end_date', 'po_status_fk']
+    
+    def form_valid(self, form):
+        form.instance.po_modifiedby = self.request.user
+        return super().form_valid(form)
+
+    
+class PurOrdersDeleteView(LoginRequiredMixin, DeleteView):
+    model = PurchaseOrder
+    success_url = '/'
