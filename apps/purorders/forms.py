4e57c17f0c88369da6e8db47bb6a01189094db35
DFrost55555 <darren.frost55555@outlook.com>
2020-01-14 16:29:05 +0000

    Update forms.py

--- apps/purorders/forms.py
+++ apps/purorders/forms.py
@@ -1,47 +1,38 @@
 from django import forms
-from .models import Project, POStatus, POType, PONote, POMatrix, POFiles
+from .models import PurchaseOrder, POStatus, POType, PONote, POMatrix, POFiles
 from apps.customers.models import Customer
 from apps.suppliers.models import Supplier, SupplierStatus
 from apps.projects.models import Project
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, CharField, DecimalField, NumberInput
 from djmoney.models.fields import MoneyField
 
+class DatePicker(forms.DateInput):
+    input_type = 'date'
+
 class PurOrderModelForm(forms.ModelForm):
-    po_reference = forms.CharField(widget=forms.TextInput(), required=True)
-    
-    project_customer_fk = ModelChoiceField(queryset=Customer.objects.all(), initial=0, required=True)
-    project_reference = forms.CharField(widget=forms.TextInput(), required=True)
-    project_chargecode = forms.CharField(widget=forms.TextInput(), required=True)
-    project_chargecodetype_fk = ModelChoiceField(queryset=ChargeCodeType.objects.all(), initial=0, required=True)
-    project_statustype_fk = ModelChoiceField(queryset=ProjectStatus.objects.all(), initial=0, required=True)
+    po_reference = forms.CharField(label="PO Reference", widget=forms.TextInput(), required=True)
+    po_quantity = forms.DecimalField(label="Quantity", widget=forms.NumberInput(), required=True)
+    po_quantity_type_fk = ModelChoiceField(label="Quantity Type", queryset=POType.objects.all(), initial=0, required=True)
+    po_cost_value = MoneyField(label="Cost Value", max_digits=14, decimal_places=2, null=True, default_currency='GBP')
+    po_unit_cost = MoneyField(label="Unit Cost", max_digits=14, decimal_places=2, null=True, default_currency='GBP')
+    po_charge_value = MoneyField(label="Charges Value", max_digits=14, decimal_places=2, null=True, default_currency='GBP')
+    po_unit_charge = MoneyField(label="Unit Charge", max_digits=14, decimal_places=2, null=True, default_currency='GBP')
+    po_start_date = forms.DateField(label='Start Date', widget=DatePicker(), required=False)
+    po_end_date = forms.DateField(label='End Date', widget=DatePicker(), required=False)
+    po_status_fk = ModelChoiceField(label="Status", queryset=POStatus.objects.all(), initial=0, required=True)
+
 
     class Meta:
-        model = Project
+        model = PurchaseOrder
         fields = [
             "po_reference",
-            "project_customer_fk",
-            "project_reference",
-            "project_chargecode",
-            "project_chargecodetype_fk",
-            "project_statustype_fk",
-        ]
-
-
-class CustProjectModelForm(forms.ModelForm):
-    project_name = forms.CharField(widget=forms.TextInput(), required=True)
-    project_customer_fk = ModelChoiceField(widget=forms.HiddenInput(), queryset=Customer.objects.all(), initial=0, required=True)
-    project_reference = forms.CharField(widget=forms.TextInput(), required=True)
-    project_chargecode = forms.CharField(widget=forms.TextInput(), required=True)
-    #project_chargecodetype_fk = ModelChoiceField(queryset=ChargeCodeType.objects.all(), initial=1, required=False)
-    #project_statustype_fk = ModelChoiceField(queryset=ProjectStatus.objects.all(), initial=1, required=False)
-
-    class Meta:
-        model = Project
-        fields = [
-            "project_name",
-            "project_customer_fk",
-            "project_reference",
-            "project_chargecode",
-            #"project_chargecodetype_fk",
-            #"project_statustype_fk",
+            "po_quantity",
+            "po_quantity_type_fk",
+            "po_cost_value",
+            "po_unit_cost",
+            "po_charge_value",
+            "po_unit_charge",
+            "po_start_date",
+            "po_end_date",
+            "po_status_fk",
         ]
