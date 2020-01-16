c8f2ea90b49887718454cdeee88a836793980577
DFrost55555 <darren.frost55555@outlook.com>
2020-01-16 17:46:21 +0000

    Update settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.formtools',
+    'formtools',
     'storages',
     'django_bootstrap_breadcrumbs',
     'django_filters',
