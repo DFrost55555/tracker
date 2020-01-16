d4f9a9173ddf096c6ecbc82c3327be63f33c9a9e
DFrost55555 <darren.frost55555@outlook.com>
2020-01-16 20:02:54 +0000

    DF 20:02

--- apps/purorders/forms.py
+++ apps/purorders/forms.py
@@ -3,36 +3,40 @@ from .models import PurchaseOrder, POStatus, POType, PONote, POMatrix, POFiles
 from apps.customers.models import Customer
 from apps.suppliers.models import Supplier, SupplierStatus
 from apps.projects.models import Project
-from django.forms import ModelChoiceField, CharField, DecimalField, NumberInput
+from django.forms import ModelChoiceField
 #from djmoney.models.fields import MoneyField
-from bootstrap_datepicker_plus import DatePickerInput, TimePickerInput, DateTimePickerInput, MonthPickerInput, YearPickerInput
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Column
+#from bootstrap_datepicker_plus import DatePickerInput, TimePickerInput, DateTimePickerInput, MonthPickerInput, YearPickerInput
+#from crispy_forms.helper import FormHelper
+#from crispy_forms.layout import Layout, Submit, Row, Column
 
+
+class DatePicker(forms.DateInput):
+    input_type = 'date'
+    
 
 class PurOrderModelForm(forms.ModelForm):
-    po_reference = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Please enter the PO reference number...'}), required=True)
+    po_reference = forms.CharField(widget=forms.TextInput(), required=True)
     po_quantity = forms.DecimalField(widget=forms.NumberInput(), required=True)
     po_quantity_type_fk = ModelChoiceField(queryset=POType.objects.all(), initial=0, required=True)
     po_cost_value = forms.DecimalField(max_digits=14, decimal_places=2, localize=True)
     po_unit_cost = forms.DecimalField(max_digits=14, decimal_places=2)
     po_charge_value = forms.DecimalField(max_digits=14, decimal_places=2)
     po_unit_charge = forms.DecimalField(max_digits=14, decimal_places=2)
-    po_start_date = forms.DateField(widget=DatePickerInput(format='%d/%m/%Y'), required=True)
-    po_end_date = forms.DateField(widget=DatePickerInput(format='%d/%m/%Y'), required=True)
+    po_start_date = forms.DateField(widget=DatePicker(),, required=True)
+    po_end_date = forms.DateField(widget=DatePicker(),, required=True)
     po_status_fk = ModelChoiceField(queryset=POStatus.objects.all(), initial=0, required=True)
     
     class Meta:
         model = PurchaseOrder
         fields = [
-            'po_reference',
-            'po_quantity',
-            'po_quantity_type_fk',
-            'po_cost_value',
-            'po_unit_cost',
-            'po_charge_value',
-            'po_unit_charge',
-            'po_start_date',
-            'po_end_date',
-            'po_status_fk'
+            "po_reference",
+            "po_quantity",
+            "po_quantity_type_fk",
+            "po_cost_value",
+            "po_unit_cost",
+            "po_charge_value",
+            "po_unit_charge",
+            "po_start_date",
+            "po_end_date",
+            "po_status_fk",
         ]
